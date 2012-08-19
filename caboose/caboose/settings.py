1a249201ba163a7ce97bf4978544d86fb0158e15
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-08-19 01:40:30 -0700

    Added South for database migrations.

--- caboose/caboose/settings.py
+++ caboose/caboose/settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': '', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -149,3 +150,5 @@ LOGGING = {
         },
     }
 }
+
+from local_settings import *
